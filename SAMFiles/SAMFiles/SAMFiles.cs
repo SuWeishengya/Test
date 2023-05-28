365ebedd90d4a0496a4a3a622f7c77fa49fa906b
SuWeisheng <sumingxiang@hust.edu.cn>
2023-05-28 11:20:59 +0800

    feat: 新增遍历源文件夹所有子文件夹功能  拷贝时打印进度
    SAMFIles开发：
    
    - 新增遍历源文件夹所有子文件夹功能
    - 新增获取源文件夹的所有文件数目（获取前删除目标文件夹），拷贝时打印进度
    
    待优化：
    
    - 拷贝前可选是否删除目标文件夹，格式化
    - 优化获取源文件夹中所有文件数目方法，当目标文件夹是源文件夹或其子文件夹时，获取数目时需要跳过目标文件夹
    - 拷贝后可选是否删除源文件夹目录，注意如果目标文件夹是源文件夹子目录时的处理
    - 增加图形界面

--- SAMFiles/SAMFiles/SAMFiles.cs
+++ SAMFiles/SAMFiles/SAMFiles.cs
@@ -3,28 +3,75 @@ using System.IO;
 
 namespace SAMFiles
 {
-    class Program
+    class SAMFiles
     {
         static void Main(string[] args)
         {
-            // 设置源文件夹路径和目标文件夹路径
+            //执行分类和拷贝
+            SortAndCopy();
+        }
+
+        /// <summary>
+        /// 分类和拷贝
+        /// </summary>
+        private static void SortAndCopy()
+        {
+            // 指定源文件夹路径
             string sourceFolderPath = @"E:\Study\SAMFilesTest";
+
+            // 指定目标文件夹路径
             string targetFolderPath = @"E:\Study\SAMFilesTest\Sort";
 
-            // 遍历所有文件
-            DirectoryInfo di = new DirectoryInfo(sourceFolderPath);
-            foreach (FileInfo file in di.GetFiles())
+            // 源文件夹为当前运行目录
+            //string sourceFolderPath = Directory.GetCurrentDirectory();
+            //string targetFolderPath = sourceFolderPath + "\\Sort";
+
+            // 若目标文件夹存在，删除目标文件夹
+            if (Directory.Exists(targetFolderPath)) Directory.Delete(targetFolderPath, true);
+
+            // 指定源文件夹中总文件数
+            int allCount = Directory.GetFiles(sourceFolderPath, "*", SearchOption.AllDirectories).Length;
+            // 当前移动了多少文件 计数
+            int curCount = 0;
+
+            // 遍历所有文件和子文件夹中的文件
+            foreach (string filePath in Directory.GetFiles(sourceFolderPath, "*", SearchOption.AllDirectories))
             {
-                // 获取文件后缀名
-                string extension = file.Extension.ToLowerInvariant();
 
-                // 创建目标文件夹
-                string targetFolder = Path.Combine(targetFolderPath, extension.Substring(1), file.Name);
-                Directory.CreateDirectory(Path.GetDirectoryName(targetFolder));
+                // 判断是否为文件夹
+                if (Directory.Exists(filePath))
+                {
+                    continue; // 如果是文件夹，则跳过
+                }
 
-                // 移动文件
-                file.MoveTo(targetFolder);
+                //判断是否是目标文件夹
+                if (filePath.IndexOf(targetFolderPath) > -1)
+                {
+                    continue;//如果是目标文件夹，则跳过
+                }
+
+                // 获取文件后缀
+                string fileExtension = Path.GetExtension(filePath);
+
+                // 根据后缀名创建文件夹
+                string destinationFolder = Path.Combine(targetFolderPath, fileExtension.TrimStart('.'));
+                if (!Directory.Exists(destinationFolder))
+                {
+                    Directory.CreateDirectory(destinationFolder);
+                }
+
+                // 拷贝文件到目标文件夹
+                string fileName = Path.GetFileName(filePath);
+                string destinationPath = Path.Combine(destinationFolder, fileName);
+
+                if (File.Exists(destinationPath)) File.Delete(destinationPath);
+                File.Copy(filePath, destinationPath, true);
+
+                curCount++;
+                Console.WriteLine($"Moved file {fileName} to {destinationFolder}({curCount}/{allCount}).");
             }
+
+            Console.WriteLine("All files are sorted.");
         }
     }
 }
